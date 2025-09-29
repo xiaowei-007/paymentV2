b30fc38ca07fc97da7e0c37910039b48ac24bfdb
小韦 <302929314@qq.com>
2025-09-29 11:16:49 +0800

    包引入调整

--- src/include.php
+++ src/include.php
@@ -6,7 +6,7 @@ spl_autoload_register(function ($classname) {
     $pathname = __DIR__ . DIRECTORY_SEPARATOR;
     $filename = str_replace('\\', DIRECTORY_SEPARATOR, $classname) . '.php';
     if (file_exists($pathname . $filename)) {
-        foreach (['AliPay', 'WeChat', 'WeMini', 'WePay', 'We', 'WePay3', 'WePayPartner'] as $prefix) {
+        foreach (['AliPay', 'WeChat', 'WeMini', 'WePay', 'We', 'WePayV3', 'WePayPartner'] as $prefix) {
             if (stripos($classname, $prefix) === 0) {
                 include $pathname . $filename;
                 return true;
